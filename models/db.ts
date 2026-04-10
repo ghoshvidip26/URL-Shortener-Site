66df3dc2dffe539de1c4c270ed1d5fcad9ac349d
ghoshvidip26 <ghoshvidip26@gmail.com>
2026-04-10 20:40:36 +0530

    chore: Migration to PostgreSQL

--- models/db.ts
+++ models/db.ts
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
-const DB_URL =
-  "postgresql://neondb_owner:npg_D3eqRiJMy0aA@ep-odd-frog-anljuxzc-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+const DB_URL = process.env.DB_URL;
 const pool = new Pool({
   connectionString: DB_URL,
 });
